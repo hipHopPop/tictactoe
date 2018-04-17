98997882863ac0c5893f59bb1c9cb6d7af4fba64
hhp <hhp@email.com>
2018-04-17 18:24:37 -0400

    some progress on supervised position matching

--- src/main/java/hhp/tictactoe/learning/supervised/smart/positionmatch/Trainer.java
+++ src/main/java/hhp/tictactoe/learning/supervised/smart/positionmatch/Trainer.java
@@ -7,11 +7,40 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import hhp.tictactoe.learning.supervised.smart.startingmatch.classification.Classifier;
+import hhp.tictactoe.learning.supervised.smart.positionmatch.classification.Classifier;
 
 public class Trainer {
 
-	public static Classifier train(URL gameImagesURL) throws URISyntaxException {}
+	public static Classifier train(URL gameImagesURL) throws URISyntaxException {
+		/*
+		 * Create a new classifier instance. The context features are Strings and the
+		 * context will be classified with a String according to the featureset of the
+		 * context.
+		 */
+		Classifier classifier = new Classifier();
+
+		/*
+		 * The classifier can learn from classifications that are handed over to the
+		 * learn methods. Imagine a tokenized text as follows. The tokens are the text's
+		 * features. The category of the text will either be positive or negative.
+		 */
+		try (Stream<String> stream = Files.lines(Paths.get(gameImagesURL.toURI()))) {
+			stream.filter(line -> line.endsWith("positive")).forEach(line -> {
+				line = line.replaceAll(",positive", "");
+				String[] s = line.split(",");
+				for (int i = 0; i < s.length; i++) {
+					classifier.setDefaultWeight(i, s[i], line);
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return classifier;
+	}
 	
-	public static void main(String[] args) throws URISyntaxException {}
+	public static void main(String[] args) throws URISyntaxException {
+		URL resource = TicTacToe.class.getResource("/supervised/tic-tac-toe.data.txt");
+		Classifier classifier = Trainer.train(resource);
+		classifier.printClassification();
+	}
 }
--- src/main/java/hhp/tictactoe/learning/supervised/smart/positionmatch/classification/Classifier.java
+++ src/main/java/hhp/tictactoe/learning/supervised/smart/positionmatch/classification/Classifier.java
@@ -1,13 +1,109 @@
 package hhp.tictactoe.learning.supervised.smart.positionmatch.classification;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import hhp.util.MapUtil;
 
-public class Classifier {}
+public class Classifier {
+
+	private Map<String, Map<Integer, Map<String, Integer>>> playerPositionMap = new HashMap<>();
+	private Map<String, Integer> fmForHighestRank 	= new HashMap<>();
+	private Map<String, Integer> fmForBestRank 		= new HashMap<>();
+	public static List<String> players = Arrays.asList(new String[]{"x", "o"});
+
+	public void setDefaultWeight(int position, String player, String image) {
+		if ( ! players.contains(player)) {
+			return;
+		}
+		Map<Integer, Map<String, Integer>> positionMap = playerPositionMap.get(player);
+		if (positionMap == null) {
+			positionMap = new HashMap<>();
+		}
+		Map<String, Integer> featureMap = positionMap.get(position);
+		if (featureMap == null) {
+			featureMap = new HashMap<>();
+			featureMap.put(image, 1);
+			fmForHighestRank.put(image, 1);
+		} else {
+			Integer weight = featureMap.get(image);
+			weight = (weight == null) ? 1 : ++weight;
+			featureMap.put(image, weight);
+			fmForHighestRank.put(image, weight);
+		}
+		positionMap.put(position, featureMap);
+		playerPositionMap.put(player, positionMap);
+	}
+
+	public void increaseWeight(List<String> gameData) {
+		String gameDataCsv = gameData.stream().collect(Collectors.joining(","));
+		IntStream.range(0, gameData.size()).forEach(position -> {
+			String player = gameData.get(position);
+			if ( ! players.contains(player)) {
+				continue;
+			}
+			Map<Integer, Map<String, Integer>> positionMap = playerPositionMap.get(player);
+			if (positionMap != null) {
+				Map<String, Integer> featureMap = positionMap.get(position);
+				featureMap.keySet().stream().filter(featureKey -> featureKey.equalsIgnoreCase(gameDataCsv))
+						.forEach(featureKey -> {
+							Integer weight = featureMap.get(gameDataCsv);
+							if (weight == null) {
+								featureMap.put(gameDataCsv, 1);
+								fmForHighestRank.put(gameDataCsv, 1);
+							} else {
+								featureMap.put(gameDataCsv, ++weight);
+								fmForHighestRank.put(gameDataCsv, weight);
+							}
+						});
+				positionMap.put(position, featureMap);
+				playerPositionMap.put(player, positionMap);
+			}
+		});
+	}
+
+	public void decreaseWeight(List<String> gameData) {
+		String gameDataCsv = gameData.stream().collect(Collectors.joining(","));
+		IntStream.range(0, gameData.size()).forEach(position -> {
+			String player = gameData.get(position);
+			if (players.contains(player)) {
+				Map<Integer, Map<String, Integer>> positionMap = playerPositionMap.get(player);
+				if (positionMap != null) {
+					Map<String, Integer> featureMap = positionMap.get(position);
+					featureMap.keySet().stream().filter(featureKey -> featureKey.equalsIgnoreCase(gameDataCsv))
+							.forEach(featureKey -> {
+								Integer weight = featureMap.get(gameDataCsv);
+								if (weight == null) {
+									featureMap.put(gameDataCsv, 0);
+									fmForHighestRank.put(gameDataCsv, 0);
+								} else {
+									featureMap.put(gameDataCsv, --weight);
+									fmForHighestRank.put(gameDataCsv, weight);
+								}
+							});
+					positionMap.put(position, featureMap);
+					playerPositionMap.put(player, positionMap);
+				}
+			}
+		});
+	}
+
+	public Stream<Entry<String, Integer>> getBestRankedImages(List<String> gameData) {}
+
+	public String getHighestRankedImage() {
+		Entry<String, Integer> i = MapUtil.getHighestRankedImage(fmForHighestRank);
+		return i.getKey();
+	}
+
+	public void printClassification() {}
+	
+	@Override
+	public Classifier clone() throws CloneNotSupportedException {}
+}
